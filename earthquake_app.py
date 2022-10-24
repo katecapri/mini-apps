8aca9d3de423939698ad4e64ba9def84102664a2
katecapri <ekaterina.kulikova22@rambler.ru>
2022-10-24 12:22:16 +0300

    Added saving and unloading from db sqlite3

--- earthquake_app.py
+++ earthquake_app.py
@@ -4,7 +4,29 @@ Format for dates: YYYY-MM-DD
 Format for other fields: float with '.'
 """
 
+import sqlite3
 import requests
+
+
+def save_to_db(earthquakes):
+    conn = sqlite3.connect('earthquakes.db')
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE earthquakes (place TEXT,magnitude REAL);')
+    cursor.executemany('INSERT INTO earthquakes VALUES (?, ?)', earthquakes)
+    conn.commit()
+    conn.close()
+
+
+def print_from_db():
+    conn = sqlite3.connect('earthquakes.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM earthquakes')
+    earthquakes = cursor.fetchall()
+    for earthquake in earthquakes:
+        print(earthquake)
+    conn.commit()
+    conn.close()
+
 
 url = "https://earthquake.usgs.gov/fdsnws/event/1/query?"
 start_time = input('Enter the start time ')
@@ -23,6 +45,12 @@ response = requests.get(url, headers={'Accept': 'application/json'}, params={
     'minmagnitude': min_magnitude
 })
 data = response.json()
+list_for_db = []
 for num in range(len(data['features'])):
     print(f"{num + 1}. Place: {data['features'][num]['properties']['place']}. Magnitude:"
           f"{data['features'][num]['properties']['mag']}")
+    list_for_db.append((data['features'][num]['properties']['place'],
+                        data['features'][num]['properties']['mag']))
+
+save_to_db(list_for_db)
+print_from_db()
